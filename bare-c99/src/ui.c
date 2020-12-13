e153ecffb48caacb57847dab9924a9d973728da9
Pau Lafoz <pau@lacodornella.cat>
2020-12-13 17:26:23 +0100

    Save new value implemented. Form and field refactoring.

--- bare-c99/src/ui.c
+++ bare-c99/src/ui.c
@@ -6,12 +6,9 @@
 #include "pt.h"
 #include "pt-delay.h"
 #include "ticker.h"
-// #include "lcd.h"
 #include "lcdpcf8574.h"
-
 #include "charmaps.h"
 #include "display_items.h"
-
 #include "pin.h"
 #include "button.h"
 #include "encoder.h"
@@ -20,28 +17,25 @@
 #define REFRESH_TIME 10  //times the ticker freq (10ms)
 
 
-//UI STATUS
-#define STARTING 0
-#define MAIN_SCREEN 1
-#define MENU 2
-
 //MACHINE STATUS
 #define STOP 0
 #define WORKING 1
 
-uint8_t machine_status = STOP;
-
-//Defines the first screen to be load
-uint8_t ui_status = STARTING;
-
-uint8_t last_ui_status = 99;
-
-display_item starting_items[START_SC_ITEMS_NUM];
-display_item main_items[MAIN_SC_ITEMS_NUM];
-display_item menu_items[MENU_SC_ITEMS_NUM];
+uint8_t machine_status = STOP;      /*!< The current machine's state */
 
 
-int8_t selected_param = 99;
+//FORM IDs
+#define STARTING 0
+#define MAIN_SCREEN 1
+#define MENU 2
+
+uint8_t form_id = 99;               /*!< The loaded form id */
+bool is_form_changed = true;        /*!< True when a new form has changed and needs to be refreshed */
+field_t *form = NULL;               /*!< A pointer to the current loaded form */
+uint8_t num_of_fields = 0;          /*!< Contains the number of fields contained on the loaded form */
+
+field_t *selected_field = NULL;     /*!< A pointer to the selected field */
+uint16_t new_selected_value = 0;
 
 /**
  * Buttons 
@@ -62,109 +56,138 @@ encoder_t encoder;
 
 /**
  * @brief     Sets the selected screen status to be displayed asynchronously
+ * 
+ * @param     new_form_id The new form's identifier
  */
-void set_UI_status(uint8_t status_id) {
-  ui_status = status_id;
+void set_form(uint8_t new_form_id) {
+  form_id = new_form_id;
+  is_form_changed = true;
+  switch (new_form_id) {
+    case STARTING:
+      form = starting_form;
+      num_of_fields = STARTING_FORM_NUM;
+      break;
+    case MAIN_SCREEN:
+      form = main_form;
+      num_of_fields = MAIN_FORM_NUM;
+      break;
+    case MENU:
+      form = menu_form;
+      num_of_fields = MENU_FORM_NUM;
+      break;
+  }
 }
 
-/**
- * @brief      Gets the current screen status
- */
-uint8_t get_UI_status(void){
-  return ui_status;
-}
 
 /**
- * @brief   Prints to the screen the item value (not the label)
- * @param   item The item to print its value
+ * @brief   Prints the field's value (not the label)
+ * @param   field The field to print its value
  */
-void print_item_value(display_item *item){
-  lcd_gotoxy(item->value_col, item->value_row);
-  sprintf(value_ch, item->val_format, item->value);
+void print_field_value(field_t *field){
+  lcd_gotoxy(field->value_col, field->value_row);
+  sprintf(value_ch, field->val_format, field->value);
   lcd_puts(value_ch);
 }
 
 
 /**
- @brief     Selects the given parameter and starts blinking it asynchronously and disables any other blinking parameter to make sure that just one is selected.
- @param     param_id The selected parameter id. Use the defined macro from display_items.h
+ * @brief   Prints the field's new selected value (not the label)
+ * @param   field The field to print its value
  */
-void select_param(int8_t param_id) {
+void print_field_new_value(field_t *field){
+  lcd_gotoxy(field->value_col, field->value_row);
+  sprintf(value_ch, field->val_format, new_selected_value);
+  lcd_puts(value_ch);
+}
+
+
+/**
+ * @brief     Selects the given parameter and starts blinking it asynchronously and disables any other blinking parameter to make sure that just one is selected.
+ * @param     field_id The selected parameter id. Use the defined macro from display_items.h
+ */
+void select_field(uint8_t field_id) {
   reset_blink_counter();
-  switch (ui_status) {
-    case STARTING:
-      for (size_t i = 0; i<START_SC_ITEMS_NUM; i++) {
-        if (starting_items[i].is_blinking){     //To avoid leaving the last selected param with the blinking char
-          print_item_value(&starting_items[i]);
-        }
-        starting_items[i].is_blinking = (i == param_id);
-      }
-      break;
-    case MAIN_SCREEN:
-      for (size_t i = 0; i<MAIN_SC_ITEMS_NUM; i++) {
-        if (main_items[i].is_blinking){     //To avoid leaving the last selected param with the blinking char
-          print_item_value(&main_items[i]);
-        }
-        main_items[i].is_blinking = (i == param_id);
-      }
-      break;
-    case MENU:
-      for (size_t i = 0; i<MENU_SC_ITEMS_NUM; i++) {
-        if (menu_items[i].is_blinking){     //To avoid leaving the last selected param with the blinking char
-          print_item_value(&menu_items[i]);
-        }
-        menu_items[i].is_blinking = (i == param_id);
-      }
-      break;
-  }
-  selected_param = param_id;
-}
 
-
-void set_value_selected_param(int8_t difference) {
-  display_item *item;
-  switch (ui_status) {
-    case STARTING:
-        item = &starting_items[selected_param];
-      break;
-    case MAIN_SCREEN:
-        item = &main_items[selected_param];
-      break;
-    case MENU:
-        item = &menu_items[selected_param];
-      break;
-    default:
-      return;
-  }
-  //Calculate 
-  item->value += (difference * item->step_val);
-  
-  //Check limits (min & max)
-  if (item->value > item->max_val){
-    item->value = item->max_val;
-  } else if (item->value < item->min_val){
-    item->value = item->min_val;
+  //Previous selected field
+  if (selected_field != NULL){
+    print_field_value(selected_field);
+    selected_field->is_selected = false;
   }
 
-  item->is_changed = true;
-
-}
-
-
-void print_item(display_item *item){
-  lcd_gotoxy(item->label_col,item->label_row);
-  lcd_puts(item->label);
-  if (item->value != 255){
-    print_item_value(item);
+  //Actual selected field
+  if (field_id != 255){
+    selected_field = &form[field_id];
+    selected_field->is_selected = true;
+    new_selected_value = selected_field->value;
+  } else {
+    selected_field = NULL;
+    new_selected_value = 0;
   }
 }
 
 
-//Prints the value characters to the blinking state (full-squared character)
-void blink_item_value(display_item *item){
-  lcd_gotoxy(item->value_col, item->value_row);
-  sprintf(value_ch, item->val_format, item->value);
-  for (uint8_t i = 0; value_ch[i] != '\0'; i++) {   //************CANVIAR EL 3************
+/**
+ * @brief Unselects the current selected field. Internally selects the last value (255)
+ */
+void unselect_field(){
+  select_field(255);
+}
+
+
+/**
+ * @brief Changes the new selected value applying the increment param
+ * 
+ * @param increment The value to increment (positive or negative)
+ */
+void change_value_selected_field(int8_t increment) {
+  if (selected_field != NULL){
+    //Calculate 
+    new_selected_value += (increment * selected_field->step_val);
+    
+    //Check limits (min & max)
+    if (new_selected_value > selected_field->max_val){
+      new_selected_value = selected_field->max_val;
+    } else if (new_selected_value < selected_field->min_val){
+      new_selected_value = selected_field->min_val;
+    }
+
+    selected_field->is_changed = true;
+  }
+}
+
+
+/**
+ * @brief Save the modified value to the corresponding field's struc
+ */
+void save_selected_value(){
+  selected_field->value = new_selected_value;
+}
+
+
+/**
+ * @brief Print the corresponding field to the lcd
+ * 
+ * @param field Pointer to the field 
+ */
+void print_field(field_t *field){
+  lcd_gotoxy(field->label_col,field->label_row);
+  lcd_puts(field->label);
+  if (field->value != 255){
+    print_field_value(field);
+  }
+}
+
+
+
+/**
+ * @brief Print the blinking full-squared characters to the corresponding field
+ * 
+ * @param field Pointer to the field
+ */
+void blink_item_value(field_t *field){
+  lcd_gotoxy(field->value_col, field->value_row);
+  sprintf(value_ch, field->val_format, new_selected_value);
+  for (uint8_t i = 0; value_ch[i] != '\0'; i++) {
     lcd_data(BLINKING_CHAR);
   }
 }
@@ -180,40 +203,23 @@ void create_custom_chars(void) {
   lcd_create_custom_char(C,C_char);
 }
 
-void charge_main_items(void) {
-  main_items[VOLUME] = volume;
-  main_items[RATIO] = ratio;
-  main_items[FREQ] = freq;
-  main_items[PEAK] = peak;
-  main_items[PLATEAU] = plateau;
-  main_items[START_STOP] = start_stop;
-  main_items[START_STOP + 1] = sentinel;
-}
 
-void charge_starting_items(void) {
-  starting_items[UPC] = upc;
-  starting_items[MANRESA] = manresa;
-  starting_items[RESPIRADOR] = respirador;
-  starting_items[RESPIRADOR+1] = sentinel;
-}
-
-void charge_menu_items(void) {
-  menu_items[MENU_ID] = menu;
-  menu_items[MENU_ID+1] = sentinel;
-}
-
-void UI_init(void) {
+/**
+ * @brief Initializes the display
+ */
+void display_init(void) {
   lcd_init(LCD_DISP_ON);
   lcd_led(0); //To activate LCD backlight
 
   create_custom_chars();
   lcd_clrscr();
-  
-  charge_main_items();
-  charge_starting_items();
-  charge_menu_items();
+  set_form(STARTING);
 }
 
+
+/**
+ * @brief Initializes the buttons and the encoder
+ */
 void buttons_init(void){
   buttons[0] = btn_create(&PORTB, 1);
   buttons[1] = btn_create(&PORTB, 0);
@@ -225,7 +231,9 @@ void buttons_init(void){
 }
 
 
-
+/**
+ * @brief Update all the button status to check if the user has pressed them
+ */
 void update_buttons_status(){
   for(uint8_t i=0; i<NUM_OF_BUTTONS; i++){
     btn_update_status(&buttons[i]);      
@@ -234,89 +242,69 @@ void update_buttons_status(){
 
 
 /**
-@brief    Returns true if the button number has been clicked (rising edge detected)
-@param    btn_id  The button identifier
-@return   A boolean (true if rising edge detected)
-*/
+ * @brief   Returns true if the button number has been clicked (rising edge detected)
+ * 
+ * @param   btn_id The button identifier
+ * @return  A boolean (true if rising edge detected)
+ */
 bool is_clicked(uint8_t btn_id){
   return buttons[btn_id].status == CLICKED;
 }
 
 
-bool UI_status_changed(void){
-  return ui_status != last_ui_status;
-}
-
-
-
-void UI_print_screen(display_item screen_items[]){
-  for (uint8_t i = 0; true; i++) {
-    if (screen_items[i].label[0] == SENTINEL[0]) {
-      break;
-    }
-    print_item(&screen_items[i]);
+/**
+ * @brief Prints the whole current form to the lcd 
+ */
+void print_form(void){
+  for (uint8_t i = 0; i < num_of_fields; i++) {
+    print_field(&form[i]);
   }
+  is_form_changed = false;
 }
 
-void UI_print_changed_params(display_item screen_items[]){
-  for (uint8_t i = 0; true; i++) {
-    if (screen_items[i].label[0] == SENTINEL[0]) {
-        break;
-    } else if (screen_items[i].is_changed == true) {
-        print_item_value(&screen_items[i]);
-        screen_items[i].is_changed = false;
+
+/**
+ * @brief Searches for the changed fields inside the current form and prints its values
+ */
+void print_changed_fields(void){
+  for (uint8_t i = 0; i < num_of_fields; i++) {
+    if(form[i].is_selected){
+      if (is_blink_time()) {
+        blink_item_value(&form[i]);
+      } else if (is_unblink_time() | form[i].is_changed) {
+        print_field_new_value(&form[i]);
+        form[i].is_changed = false;    
         reset_unblink_counter();
-    } else if(screen_items[i].is_blinking){
-        if (is_blink_time()) {
-            blink_item_value(&screen_items[i]);
-        } else if (is_unblink_time()) {
-            print_item_value(&screen_items[i]);
-        }
+      }
+    } else if (form[i].is_changed) {
+      print_field_value(&form[i]);
+      form[i].is_changed = false;
+      reset_unblink_counter();
     }
   }
 }
-
 
 
 
 /*****************************************************************
  * @brief   Protothread to refresh the display if needed
  *****************************************************************/
-PT_THREAD(ui_thread(struct pt *pt))
+PT_THREAD(display_thread(struct pt *pt))
 {
   static uint16_t chronos;
 
   PT_BEGIN(pt);
 
   for(;;) {
-    if (UI_status_changed()) {
+    if (is_form_changed) {
       lcd_clrscr();
       reset_blink_counter();
-      select_param(99);
-
-      switch (ui_status) {
-        case STARTING:
-            UI_print_screen(starting_items);
-          break;
-        case MAIN_SCREEN:
-            UI_print_screen(main_items);
-          break;
-        case MENU:
-            UI_print_screen(menu_items);
-          break;
-      }
+      unselect_field();
+      print_form();
     } else {
-      switch (ui_status) {
-        case MAIN_SCREEN:
-            UI_print_changed_params(main_items);
-          break;
-        case MENU:
-            UI_print_changed_params(menu_items);
-          break;
-      }
+      print_changed_fields();
     }
 
-    last_ui_status = ui_status;
     update_blink_counter();
 
     chronos = ticker_get();
@@ -342,45 +330,46 @@ PT_THREAD(buttons_thread(struct pt *pt))
 
     update_buttons_status();
 
-    switch (ui_status) {
+    switch (form_id) {
       case STARTING:
         _delay_ms(1000);            //To be changed. Simulates the homing sequence.
-        set_UI_status(MAIN_SCREEN);
+        set_form(MAIN_SCREEN);
         break;
       case MAIN_SCREEN:
         if (is_clicked(BTN_1)){
-          select_param(VOLUME);
+          select_field(VOLUME);
         } else if (is_clicked(BTN_2)){
-          select_param(RATIO);
+          select_field(RATIO);
         } else if (is_clicked(BTN_3)){
-          select_param(FREQ);
+          select_field(FREQ);
         } else if (is_clicked(BTN_MENU)){
-          set_UI_status(MENU);
-        } else if (is_clicked(BTN_ENC) & (selected_param != 99) ){
-          select_param(99);
+          set_form(MENU);
+        } else if (is_clicked(BTN_ENC) & (selected_field != NULL) ){
+          save_selected_value();
+          unselect_field();
         } else if (is_clicked(BTN_START)){  
           if (machine_status == STOP){                  //*** Start cycle ***
-            main_items[START_STOP].val_format = STOP_ST;
-            main_items[PLATEAU].val_format = "%u";
-            main_items[PEAK].val_format = "%u";
+            main_form[START_STOP].val_format = STOP_ST;
+            main_form[PLATEAU].val_format = UINT_FORMAT;
+            main_form[PEAK].val_format = UINT_FORMAT;
             machine_status = WORKING;
           } else if (machine_status == WORKING){        //*** Stop cycle ***
-            main_items[START_STOP].val_format = START_ST;
-            main_items[PLATEAU].val_format = OFF_ST;
-            main_items[PEAK].val_format = OFF_ST;
+            main_form[START_STOP].val_format = START_ST;
+            main_form[PLATEAU].val_format = OFF_ST;
+            main_form[PEAK].val_format = OFF_ST;
             machine_status = STOP;
           }         
-          main_items[START_STOP].is_changed = true;
-          main_items[PLATEAU].is_changed = true;
-          main_items[PEAK].is_changed = true;
+          main_form[START_STOP].is_changed = true;
+          main_form[PLATEAU].is_changed = true;
+          main_form[PEAK].is_changed = true;
         } else if (get_position(&encoder) != 0){
-          set_value_selected_param(get_position(&encoder));
+          change_value_selected_field(get_position(&encoder));
           reset_position(&encoder);
         }
         break;
       case MENU:
         if (is_clicked(BTN_MENU)){
-          set_UI_status(MAIN_SCREEN);
+          set_form(MAIN_SCREEN);
         }
         break;
     }
@@ -411,25 +400,25 @@ PT_THREAD(encoder_thread(struct pt *pt))
 
 int main(void) {
   /* context dels threads */
-  struct pt ui_ctx, buttons_ctx, encoder_ctx;
+  struct pt display_ctx, buttons_ctx, encoder_ctx;
 
   /* init modules */
   ticker_setup();
   ticker_start();
-  UI_init();
+  display_init();
   buttons_init();
   sei();
 
   /* init contexts */
-  PT_INIT(&ui_ctx);
+  PT_INIT(&display_ctx);
   PT_INIT(&buttons_ctx);
   PT_INIT(&encoder_ctx);
 
   /* do schedule of threads */
   for(;;) {
-    (void)PT_SCHEDULE(ui_thread(&ui_ctx));
     (void)PT_SCHEDULE(buttons_thread(&buttons_ctx));
     (void)PT_SCHEDULE(encoder_thread(&encoder_ctx));
+    (void)PT_SCHEDULE(display_thread(&display_ctx));
   }
   return 0;
 }
